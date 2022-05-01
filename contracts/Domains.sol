e039e162aabac22c8125f8e99a3b972305dc8694
Prince Codes <princecodes247@gmail.com>
2022-05-01 19:14:16 -0400

    feat: update require error messages

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -7,19 +7,32 @@ import "hardhat/console.sol";
 contract Domains {
   // A "mapping" data type to store their names
   mapping(string => address) public domains;
+    // Checkout our new mapping! This will store values
+  mapping(string => string) public records;
 
   constructor() {
       console.log("GBALAM DOMAIN CONTRACT.");
   }
 
-  // A register function that adds their names to our mapping
   function register(string calldata name) public {
+      // Check that the name is unregistered (explained in notes)
+      require(domains[name] == address(0), "Domain already registered");
       domains[name] = msg.sender;
       console.log("%s has registered a domain!", msg.sender);
   }
 
-  // This will give us the domain owners' address
   function getAddress(string calldata name) public view returns (address) {
+      // Check that the owner is the transaction sender
       return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender, "You are not the owner of this domain!");
+      records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
   }
 }
